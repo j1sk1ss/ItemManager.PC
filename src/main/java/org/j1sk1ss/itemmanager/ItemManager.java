b29761d3df6d93e0df051d236f08435655548651
j1sk1ss <fotikolay222@gmail.com>
2024-04-28 11:40:06 +0500

    Initial commit

---
+++ src/main/java/org/j1sk1ss/itemmanager/ItemManager.java
@@ -0,0 +1,17 @@
+package org.j1sk1ss.itemmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/org/j1sk1ss/itemmanager/manager/Manager.java
@@ -0,0 +1,2 @@
+package org.j1sk1ss.itemmanager.manager;public class Manager {
+}
